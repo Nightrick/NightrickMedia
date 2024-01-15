35a41051c4960a431591291d513c833557f0deb1
David <brushdm@gmail.com>
2024-01-14 23:37:56 -0500

    Adding Cloudscape Components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,94 +1,69 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import React from 'react';
+import {
+  AppLayout,
+  BreadcrumbGroup,
+  Container,
+  ContentLayout,
+  Header,
+  HelpPanel,
+  Link,
+  SideNavigation,
+} from '@cloudscape-design/components';
 
 export default function Home() {
+
+  let [helpPanelOpen, setHelpPanelOpen] = React.useState(false);
+  let [navPanelOpen, setNavPanelOpen] = React.useState(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Welcome to Nightrick Media
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <AppLayout
+      breadcrumbs={
+        <BreadcrumbGroup
+          items={[
+            { text: 'Home', href: '#' },
+            { text: 'Nightrick Media', href: '#' },
+          ]}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      }
+      navigationOpen={navPanelOpen}
+      navigation={
+        <SideNavigation
+          header={{
+            href: '#',
+            text: 'Nightrick Media',
+          }}
+          items={[
+            { type: 'link', text: `Publishing`, href: `#` },
+            { type: 'link', text: `YouTube`, href: `#` },
+            { type: 'link', text: `TikTok`, href: `#` },
+          ]}
+        />
+      }
+      onNavigationChange={() => {setNavPanelOpen(!navPanelOpen)}}
+      toolsOpen={helpPanelOpen}
+      tools={<HelpPanel header={<h2>We're here to help</h2>}>Nightrick Media is there for you.</HelpPanel>}
+      onToolsChange={()=> {setHelpPanelOpen(!helpPanelOpen)}}
+      content={
+        <ContentLayout
+          header={
+            <Header variant="h1">
+              Nightrick Media
+            </Header>
+          }
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <Container
+            header={
+              <Header variant="h2">
+                Please select from one of our many offerings:
+              </Header>
+            }
+          >
+            Induction
+          </Container>
+        </ContentLayout>
+      }
+    />
+  );
 }
